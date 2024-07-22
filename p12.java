8f7b8f7a8d9467c5b768fba8bf49175db712830f
SHREJAL UPADHYAY <147235491+ShrejalUpa@users.noreply.github.com>
2024-07-22 12:05:43 +0530

    Add files via upload

---
+++ p12.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class p12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your Name Dear😀🤖");
+        String s = sc.next();
+        System.out.println("Oww Wow Nice to see you here " +s+" ..Always learn somthing new Dear!!!");
+    }
+}
+
+
+// Okey So learn here With Shrejal 
+// Class - A user Defiend Blueprint consisting of attrinbutes and methods For Example "Car"
+// here Attributr is Company name , color , price That all are the Attribute names 
+// And Method is here like run() , OpenTrunk() , OpenWindow(), That all are the Methods 
+// And Most Important Here Object Or Instanses is  BMW , AUDI , MERCEDES That all the Instances of Class
+
+
+// Now How to define a method of "CAR"
+// So you can define it As "BMW.color" , "AUDI.price" , "MERCEDESE.OpenTrunk()"  Okey Got it!!!
+
+
+// What is the Main Method? 
+// This is the ENtry Piont in a JAva Progrraming, Function Name.
+
+//What is Void?
+// This is the Return tyep, Void That means null or can say nothing.
+
+//What is public?
+// This is The Access Modifier, This can be use anywhere..
+//Static:- It Allows to call this method without creating any object of class
+//(String[] args):- Command line Argument(Default)
+//ln:- new line 
+// System:- It always present of all java files it is nothing but a system class
+//out:- Object of print Stream
+//println():- method in print stram class
+
+
+
+
+//Hope you larn Somthing New!!!!!😊👍
