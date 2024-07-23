e181248db98f870ec68ef17d112e9270e5a12d47
SHREJAL UPADHYAY <147235491+ShrejalUpa@users.noreply.github.com>
2024-07-23 11:23:49 +0530

    Add files via upload

---
+++ p13.java
@@ -0,0 +1,14 @@
+//Package- lowercase
+import java.util.*;
+public class P13{      //Class-PascalCase 
+    public static void main(String[] args){
+
+    }
+}
+
+
+//Constant value- UPPER_SNAKE_CASE
+//Function & Variable - camelCAse
+//Rule1- Variable names should not begin with a number
+//Rule2- Whitespace is not permitted
+//Rule3- A java keyword cannot be used as a varible name
