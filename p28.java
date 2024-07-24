863568a4b8490f12eaf72979ab9e55b43eef33f0
SHREJAL UPADHYAY <147235491+ShrejalUpa@users.noreply.github.com>
2024-07-24 20:12:38 +0530

    Add files via upload

---
+++ p28.java
@@ -0,0 +1,27 @@
+//Ques:- WHat is the output of the following code:
+// int x = 60 , y = 90;
+// if(y%x == 10);
+//     System.out.println("Good")
+// else if(y-x==30)
+//     System.out.println("Natural");
+// else
+//     System.out.println("bad")
+
+
+public class p28 {
+    public static void main(String[] args){
+        int x = 60 , y = 90;
+        if(y%x == 10)
+            System.out.println("Good");
+        else if(y-x==30)
+            System.out.println("Natural");
+        else
+            System.out.println("bad");
+
+    }
+}
+
+
+
+
+//Guess the output first.....
---
+++ p29.java
@@ -0,0 +1,21 @@
+//Ques:- What is the output of the folowing snippet?
+// int x = 60 , y = 90;
+// if(y*x==540 && x+y==150)
+//     System.out.println("Good");
+// else
+//     System.out.println("bad");
+
+
+public class p29 {
+    public static void main(String[] args){
+        int x = 60 , y = 90;
+        if(y*x==540 && x+y==150)
+            System.out.println("Good");
+        else
+            System.out.println("bad");
+
+    }
+}
+
+
+// Guess the output first....
