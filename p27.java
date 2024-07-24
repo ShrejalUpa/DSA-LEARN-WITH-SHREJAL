cca1d8a876b93f6e7078a53d8c967afd5172ab6a
SHREJAL UPADHYAY <147235491+ShrejalUpa@users.noreply.github.com>
2024-07-24 20:03:01 +0530

    Add files via upload

---
+++ p27.java
@@ -0,0 +1,17 @@
+//Ques:- What is output of the following code:
+// int x = 60 , y = 90;
+// if(y%x == 0);
+//     System.out.println("Good")
+// else
+//     System.out.println("Bad");
+
+public class p27 {
+    public static void main(String[] args){
+        int x = 60 , y = 90;
+            if(y%x == 0)
+                System.out.println("Good");
+            else
+                System.out.println("Bad");
+
+    }
+}
