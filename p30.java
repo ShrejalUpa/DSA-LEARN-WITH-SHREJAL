f501a6c205533d33039bb6f5ecbd9c8eba45eb74
SHREJAL UPADHYAY <147235491+ShrejalUpa@users.noreply.github.com>
2024-07-25 20:18:58 +0530

    Add files via upload

---
+++ p30.java
@@ -0,0 +1,19 @@
+//Ques:-  What is the output of the following snippet:
+// int x = 60, y = 90;
+// if(y * x == 540 || x + y == 150)
+// System.out.println("Gppd");
+// else
+// System.out.println("Bad");
+
+public class p30 {
+  public static void main(String[] args) {
+    int x = 60, y = 90;
+    if(y * x == 540 || x + y == 150)
+        System.out.println("G00d");
+    else
+        System.out.println("Bad");
+
+  }  
+
+  //Guess the output first.....#learnWithShrejal😊👍
+}
