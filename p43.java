13bec8a6002474b88a7bbf44f49095f9d52628f0
SHREJAL UPADHYAY <147235491+ShrejalUpa@users.noreply.github.com>
2024-07-28 21:48:44 +0530

    Rectangular📈

---
+++ p43.java
@@ -0,0 +1,18 @@
+
+//Ques:- Pattern printing:  Rectangular Pattern
+
+import java.util.Scanner;
+
+public class p43 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        for(int i = 1; i <= r; i++) {
+            for(int j = 1; j <= c; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
