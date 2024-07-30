f3e9bceabd8cf28151611cf5c07d66b044b427bb
SHREJAL UPADHYAY <147235491+ShrejalUpa@users.noreply.github.com>
2024-07-30 22:47:29 +0530

    OOPs!!!!

---
+++ p53.java
@@ -0,0 +1,56 @@
+//OOPs 
+//Ques: Create a class named "Person" with a variable age.
+
+
+public class p53 {
+    public static void main(String[] args) {
+        int age = 20;
+    }
+    
+}
+
+
+//Creating an Object:-
+// Classname ObjectName = new ClassName();
+// example: Car BMW = new car();
+
+// Now Questionis that and you have to solve in your mind " create an Object of the person class"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//So okey i am giving solution here:-
+
+//  public class Person{
+//     int age = 20;
+//     public static void main(String args[]);
+//     Person Rohan = new Person();
+//  }
+
+
+
+
+
+
+
+
+
+//Here we can accesss amy attribute or method by using .(dot) operator
+//example: sout(Rohan.age);
