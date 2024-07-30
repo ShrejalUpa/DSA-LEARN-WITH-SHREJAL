dd489dd5da839f253d68613740783b0f58509d7c
SHREJAL UPADHYAY <147235491+ShrejalUpa@users.noreply.github.com>
2024-07-30 22:54:32 +0530

    Multiple class

---
+++ p54.java
@@ -0,0 +1,14 @@
+//Now lets see how to crate multiple classes
+
+
+class student{
+    int rollNo;
+    String studentName;
+}
+public class p54{
+    public static void main(String[] args) {
+        student obj1 = new student();
+        System.out.println(obj1.rollNo);
+        System.out.println(obj1.studentName);
+    }
+}
