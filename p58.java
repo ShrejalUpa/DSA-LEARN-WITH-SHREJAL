bb2ff515c227e977c10f13f515e9037c6a8d8c34
SHREJAL UPADHYAY <147235491+ShrejalUpa@users.noreply.github.com>
2024-07-31 23:18:16 +0530

    Math Library

---
+++ p58.java
@@ -0,0 +1,14 @@
+//Math library: -
+// sout(Math.sqrt(24));
+// sout(Math.floor(5.3));
+// sout(Math.ceil(5.8));
+
+
+public class p58 {
+    public static void main(String[] args) {
+        System.out.println(Math.ceil(5.9));
+        System.out.println(Math.floor(5.9));
+        System.out.println(Math.sqrt(5.9));
+
+    }
+}
