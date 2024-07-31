c07166885b2849dab5365dc7af0fec6d23d7d099
SHREJAL UPADHYAY <147235491+ShrejalUpa@users.noreply.github.com>
2024-07-31 22:22:37 +0530

    Methods^_~

---
+++ p55.java
@@ -0,0 +1,39 @@
+// Calling a Methods
+public class p55 {
+    static void welcome(){          //here static is non access modifier
+        System.out.println("Welcome ji Welcome!!");
+    }
+    public static void main(String[] args) {
+        welcome();                  // here we call that method 
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+//Ques : Now recall the ans in your mind What of the operation is used to allocate memory for an object in java?(new)
+//Ques : Give the in in true or false  Object of the same class habe different properties ?(f)
+//Ques : Object is an instance Of a class (t) 
+//Ques: A classis a user defiend blueprint from which objects are created? (t)
+//Ques: Creating an object from a class is known as: (instantiating)
+// java methods:-  java method is block of code which performing some action which runs only when it is called.
+// Example: functions :- bark();
+//          Methods :- fluffy.bark();
+//Two types of method 
+//(i) User defined method:- which we are making 
+//(ii) Standard library Method:- Some build in methods in java which we can use like sqet(), floor() etc...
+//How to declare a method?
+// public int sum(int a , int b)   this is the method declaration it also called as method method header
+//Now lets see What comes inside the method header..
+//Method signature:- sum{which is methodname}(int a , int b){And this is parameter list}
+//Access Specifier
+//return type
+//method name
+//parameters(int a,int b)
