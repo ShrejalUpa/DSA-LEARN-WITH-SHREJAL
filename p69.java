10e2543ed027c91421223ae49dcb133b041b6167
SHREJAL UPADHYAY <147235491+ShrejalUpa@users.noreply.github.com>
2024-08-03 21:41:49 +0530

    Practice

---
+++ p69.java
@@ -0,0 +1,8 @@
+//Array:-It is a collection of similar types of data or homogeneous items, array having 0 based indexing
+
+public class p69 {
+    public static void main(String[] args) {
+        int[] ages;
+        ages = new int[10]; // You need to specify the size of the array
+    }
+}
