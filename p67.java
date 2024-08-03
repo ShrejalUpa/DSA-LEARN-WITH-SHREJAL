95a10ce07a793b4fba5390d94557e55047a3f81c
SHREJAL UPADHYAY <147235491+ShrejalUpa@users.noreply.github.com>
2024-08-03 21:23:56 +0530

    Practice

---
+++ p67.java
@@ -0,0 +1,15 @@
+// Guess the output:-
+
+public class p67 {
+    public static void makeTwice(int p) {
+        p=p*2;
+    }
+    public static void main(String[] args) {
+        int p = 26;
+        makeTwice(p);
+        System.out.println(p);        
+    }
+}
+
+
+//In java there is pass by value so value does not change
