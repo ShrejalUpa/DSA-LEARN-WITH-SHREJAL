87bb2f6700b18b949e527bccf7ca554b72fcbfa4
SHREJAL UPADHYAY <147235491+ShrejalUpa@users.noreply.github.com>
2024-08-04 22:24:28 +0530

    Array Literals

---
+++ p70.java
@@ -0,0 +1,8 @@
+
+//Array Literals:-With curly braces we can initialise the array and add value to it during initialization without defining the size.
+
+public class p70 {
+    public static void main(String[] args) {
+        int[] intArray = {1,2,3,4,5,6,7,8,9,10};
+    }
+}
