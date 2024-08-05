eaf63a2cc0b07fdcf9d69ab0ef29264b3f6422f6
SHREJAL UPADHYAY <147235491+ShrejalUpa@users.noreply.github.com>
2024-08-05 21:40:50 +0530

    Traversing

---
+++ p75.java
@@ -0,0 +1,36 @@
+
+//Traversing through the Array:- We can use loops to traverse through the array.There are many ways to iterate over the Array. The most common way of looping through arrays in java are:-For loop, For each loop,  While loop./**
+
+
+public class p75 {
+    public static void main(String[] args) {
+
+        //   For loop
+
+        // int ages[] = new int[4];
+        // for(int i=0; i<300; i++){
+        //     System.out.println(ages[i]);
+        // }
+
+
+
+        //      For each loop
+
+
+        // int ages[] = new int[4];
+        // for(int age:ages){
+        //     System.out.println(ages);
+        // }
+
+
+
+        // While-loop
+
+        int ages[] = new int[4];
+        int i =0;
+        while(i<3){
+            System.out.println(ages[i]);
+            ++i;
+        }
+    }
+}
