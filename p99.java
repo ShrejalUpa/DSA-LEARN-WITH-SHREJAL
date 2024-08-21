58744041ac0f8111f670ec1335d499f5b6f1940b
SHREJAL UPADHYAY <147235491+ShrejalUpa@users.noreply.github.com>
2024-08-21 23:45:36 +0530

    2D array

---
+++ p99.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class p99 {
+    static void printArray(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " "); // Changed from println to print for better formatting
+            }
+            System.out.println(); // Moved this to after the inner loop to print each row on a new line
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of rows:");
+        int r = sc.nextInt();
+        System.out.println("Enter number of columns:");
+        int c = sc.nextInt();
+
+        int[][] arr = new int[r][c];
+        System.out.println("Enter " + r * c + " elements:");
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        printArray(arr);
+    }
+}
